1debf72fb11d6713649c5036325d1831ca4f5120
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2023-04-17 16:01:41 +0200

    admin

--- routes/web.php
+++ routes/web.php
@@ -24,13 +24,13 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::prefix('user')->name('user.')->group(function(){
-    Route::middleware(['guest'])->group(function(){
+    Route::middleware(['guest:web'])->group(function(){
         Route::view('/login', 'dashboard.user.login')->name('login');
         Route::view('/register', 'dashboard.user.register')->name('register');
         Route::post('/create', [UserController::class, 'create'])->name('create');
         Route::post('/dologin', [UserController::class, 'dologin'])->name('dologin');
     });
-    Route::middleware(['auth'])->group(function(){
+    Route::middleware(['auth:web'])->group(function(){
         Route::view('/home', 'dashboard.user.home')->name('home');
         Route::get('/logout', [UserController::class, 'logout'])->name('logout');
     });
